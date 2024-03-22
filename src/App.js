2349d07f0a3ee63e94176d5f90fdc35b120ade52
pulkitnetsmartz <161713324+pulkitnetsmartz@users.noreply.github.com>
2024-03-22 16:18:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Docker se app chal gya
         </p>
         <a
           className="App-link"
